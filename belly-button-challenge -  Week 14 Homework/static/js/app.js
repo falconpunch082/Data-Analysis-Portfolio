e63b1da9f7de3b034ec580b1067fa7b685bcd59d
falconpunch082 <dale.tanoto13@gmail.com>
2023-12-03 09:27:52 +1100

    Made zipping, sorting and unzipping code

--- belly-button-challenge -  Week 14 Homework/static/js/app.js
+++ belly-button-challenge -  Week 14 Homework/static/js/app.js
@@ -18,11 +18,37 @@ d.then(function(d){
         td.push({
             name: d.names[i],
             metadata: d.metadata[i],
-            otu_ids: d.samples[i].otu_ids,
-            sample_values: d.samples[i].sample_values,
-            otu_labels: d.samples[i].otu_labels
+            samples: d.samples[i]
+            //otu_ids: d.samples[i].otu_ids,
+            //sample_values: d.samples[i].sample_values,
+            //otu_labels: d.samples[i].otu_labels
         });
     }
 
+    /*
+    to sort based on values
+    - combine(zip) each id, value and label together to become a list of key-value pairs
+    - sort them based on value
+    - unzip them back to previous format for plotly to read
+    with this you can then filter out top 10 and assign to new variable
+    */
+
+    for (let i = 0; i < td.length; i++){
+        let s = td[i].samples;
+      
+        let zippedSamples = s.otu_ids.map((otu_id, index) => ({
+            id: otu_id, 
+            value: s.sample_values[index], 
+            label: s.otu_labels[index]
+        }))
+    
+        zippedSamples.sort((a,b) => b.value - a.value);
+    
+        td["otu_ids"] = zippedSamples.map(item => item.id);
+        td["sample_values"] = zippedSamples.map(item => item.value);
+        td["otu_labels"] = zippedSamples.map(item => item.label);
+    }
+    
     console.log(td);
+
 });
